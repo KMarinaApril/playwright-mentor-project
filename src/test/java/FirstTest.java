004bdd31eb100c8991ad405e9e788925e5e93c39
Marina Korolenko <marina.korolenko@gmail.com>
2026-02-26 16:27:09 +0100

    feat: first successful run of playwright test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,22 @@
-public class FirstTest{
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void myFirstTest() {
+        try (Playwright playwright = Playwright.create()) {
+            // Запускаем браузер в видимом режиме (headless = false)
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+
+            // Переходим на сайт
+            page.navigate("https://playwright.dev/");
+
+            // Печатаем заголовок в консоль
+            System.out.println("Заголовок страницы: " + page.title());
+
+            // Закрываем браузер
+            browser.close();
+        }
+    }
 }
