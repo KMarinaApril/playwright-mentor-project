fe96317396facdd7e0ced6d544ddac4054d4e800
Marina Korolenko <marina.korolenko@gmail.com>
2026-02-27 15:19:27 +0100

    feat: Get Started page title

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,17 +5,23 @@ public class FirstTest {
     @Test
     void myFirstTest() {
         try (Playwright playwright = Playwright.create()) {
-            // Запускаем браузер в видимом режиме (headless = false)
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
 
-            // Переходим на сайт
+            // 1. Идем на сайт
             page.navigate("https://playwright.dev/");
 
-            // Печатаем заголовок в консоль
-            System.out.println("Заголовок страницы: " + page.title());
+            // 2. Нажимаем на кнопку поиска (она находится по тексту или классу)
+            page.locator(".DocSearch-Button").click();
 
-            // Закрываем браузер
+            // 3. Вводим слово "Locators" в появившееся поле
+            page.locator("#docsearch-input").fill("Locators");
+
+            // 4. Ждем секунду, чтобы увидеть результат (в реальных тестах так не делают, но нам для красоты надо)
+            page.waitForTimeout(2000);
+
+            System.out.println("Поиск успешно выполнен!");
+
             browser.close();
         }
     }
---
+++ src/test/java/GetStarted.java
@@ -0,0 +1,32 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetStarted {
+    @Test
+    void myFirstTest() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+
+            // 1. Идем на сайт
+            page.navigate("https://playwright.dev/");
+
+            // 2. Нажимаем на кнопку Get Started (она находится по тексту или классу)
+            page.getByText("Get Started").first().click();
+
+            // 3. Ждем секунду, чтобы увидеть результат (в реальных тестах так не делают, но нам для красоты надо)
+            page.waitForTimeout(2000);
+
+            // Проверяем, что заголовок совпадает с ожидаемым
+            assertEquals("Installation | Playwright", page.title());
+
+            System.out.println ("Заголовок страницы: " + page.title());
+
+            browser.close();
+        }
+    }
+}
